196ee42ebc3e54ab9a7fb620611e13cf57e259fa
T7Q <tkuumola@gmail.com>
2020-06-17 13:09:10 +0300

    test: :white_check_mark: update test

--- src/tests/handSubmit.test.js
+++
@@ -1,7 +0,0 @@
-import { handleSumbit } from '../client/js/app';
-
-describe('search on submit', () => {
-  test('It should return true', () => {
-    expect(handleSubmit).toBeDefined();
-  });
-});
